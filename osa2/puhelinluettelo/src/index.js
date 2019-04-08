b087ab7ded96b42f51462e94b205d25d57e00c10
Joni Hiltunen <djonih@gmail.com>
2019-04-08 11:40:43 +0300

    2e: tyylit

--- osa2/puhelinluettelo/src/index.js
+++ osa2/puhelinluettelo/src/index.js
@@ -1,7 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
+import './index.css';
+
 import personService from './services/persons';
+
+const Notification = ({ notification }) => {
+  const { message, type } = notification;
+
+  if (message === null) {
+    return null;
+  }
+  if (type === 'notification') {
+    return <div className="notification">{message}</div>;
+  } else if (type === 'error') {
+    return <div className="error">{message}</div>;
+  } else {
+    console.log('Notification: wrong type', type, 'message was', message);
+    return null;
+  }
+};
 
 const Person = ({ person }) => (
   <div>
@@ -58,6 +76,10 @@ const App = () => {
   const [filter, setFilter] = useState('');
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [notification, setNotification] = useState({
+    message: null,
+    type: null,
+  });
 
   useEffect(() => {
     personService.getAll().then(res => {
@@ -78,6 +100,13 @@ const App = () => {
     setFilter(e.target.value);
   };
 
+  const showNotification = (notification, length = 3000) => {
+    setNotification(notification)
+    setTimeout(() => {
+      setNotification({ message: null, type: null })
+    }, length)
+  }
+
   const onDeleteClick = id => () => {
     console.log('deleteClick', id);
     const { name } = persons.find(p => p.id === id);
@@ -88,6 +117,10 @@ const App = () => {
         let newPersons = [...persons];
         newPersons = newPersons.filter(p => p.name !== name);
         setPersons(newPersons);
+        showNotification({
+          message: `Poistettiin ${name}`,
+          type: 'notification'
+        }, 3000);
       });
     }
   };
@@ -103,20 +136,33 @@ const App = () => {
     const found = persons.find(person => person.name === newName);
     if (found) {
       // update existing person
-      const confirmation = window.confirm(`${found.name} on jo luettelossa, korvataanko numero uudella?`);
+      const confirmation = window.confirm(
+        `${found.name} on jo luettelossa, korvataanko numero uudella?`
+      );
       if (confirmation) {
-        personService
-          .update(found.id, newPerson)
-          .then(res => {
-            console.log('update res', res)
-            let newPersons = [...persons];
-            const { name, number, id } = res.data;
-            const index = newPersons.indexOf(found);
-            newPersons[index] = { name, number, id }
-            setPersons(newPersons)
-            setNewName('')
-            setNewNumber('')
-          })
+        personService.update(found.id, newPerson).then(res => {
+          console.log('update res', res);
+          let newPersons = [...persons];
+          const { name, number, id } = res.data;
+          const index = newPersons.indexOf(found);
+          newPersons[index] = { name, number, id };
+          setPersons(newPersons);
+          setNewName('');
+          setNewNumber('');
+          showNotification({
+            message: `Päivitettiin ${name}, uusi numero on ${number}`,
+            type: 'notification'
+          }, 3000)
+        }).catch(error => {
+          // the person to update has been deleted already
+          showNotification({
+            message: `Henkilö ${found.name} oli jo poistettu`,
+            type: 'error'
+          }, 3000)
+          let newPersons = [...persons];
+          newPersons = newPersons.filter(p => p.name !== found.name);
+          setPersons(newPersons);
+        })
       }
     } else {
       // add a new person
@@ -127,6 +173,10 @@ const App = () => {
         setPersons(newPersons);
         setNewName('');
         setNewNumber('');
+        showNotification({
+          message: `Lisättiin ${name} numerolla ${number}`,
+          type: 'notification'
+        }, 3000);
       });
     }
   };
@@ -134,7 +184,7 @@ const App = () => {
   return (
     <div>
       <h2>Puhelinluettelo</h2>
-
+      <Notification notification={notification} />
       <InputForm filter={filter} handleFilterChange={handleFilterChange} />
       <h3>Lisää uusi</h3>
       <PersonForm
